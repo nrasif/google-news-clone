46747769ad197889fcc94d139f9b83960c930cf2
naharirasif <naharirasif18@gmail.com>
2024-07-19 11:23:03 +0700

    Creating menubar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar"
+import Menubar from "./components/Menubar"
 
 export default function App() {
   return (
     <>
     
       <Navbar/>
-
+      <Menubar/>
+      
     </>
   )
 }
